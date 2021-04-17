f300765587c14a0a485425283b3233c162bf35c4
Ramiro Martinez <ramiro.daniel.ing@gmail.com>
2021-04-17 20:31:12 -0300

    feat(pm2, readme): improve production run and added readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# cripto-b-alert
-telegram bot to get information of binance api
+# Cripto Binance Alert
+Telegram bot to get binance info and set alarms (soon)
+
+# Installation 
+## We need the following dependencies:
+
+`$sudo npm i -g npm`
+`$sudo npm i -g pm2`
+
+## Download the code:
+
+`$git clone https://github.com/ramyx/cripto-b-alert.git`
+`$cd cripto-b-alert`
+
+## Set up the environment:
+Create an ".env" file and set the Telegram Bot Token:
+
+`BOT_TOKEN=<place-here-your-bot-token>`
+
+# Execution
+For development:
+
+`$npm start`
+
+For production (no need for server works fine locally):
+
+`$npm run serve`
+
+# Stop/Restart
+
+`$npm run serve-stop`
+`$npm run serve-restart`
--- src/bot.ts
+++ src/bot.ts
@@ -6,21 +6,10 @@ const bot = new TelegramBot(ENV.BOT_TOKEN, { polling: true });
 
 const client = Binance()
 
-// bot.onText(/\/init/, async (msg) => {
-//     console.log(msg);
-//     bot.sendMessage(msg.chat.id, 
-//         "Hello crypto man :).\nThanks for using me.\nNow i will try to connect to binance api...");
-//     //anything
-//     console.log(await client.prices({ symbol: 'BTCBUSD' }))
-//     console.log(await client.avgPrice({ symbol: 'BTCBUSD' }))
-
-// });
-
 bot.onText(/\/test/, async (msg) => {
     console.log(msg);
     bot.sendMessage(msg.chat.id,
         "\nI will try to connect to binance api...");
-    //anything
     if (await client.ping()) {
         bot.sendMessage(msg.chat.id,
             "\nEverything looks good :)");
@@ -31,19 +20,15 @@ bot.onText(/\/test/, async (msg) => {
     console.log(await client.ping())
 });
 
-bot.onText(/\/tothemoon/, async (msg) => {
-    const changePercent = 10;
-    //anything
+bot.onText(/\/tothemoon (.+)/, async (msg, match) => {
+    const changePercent = isNaN(parseInt(match[1])) ? 10 : parseInt(match[1]);
     const dailyStats = await client.dailyStats() as DailyStatsResult[];
     dailyStats.map(ds => {
         if (ds.symbol.search('BUSD') !== -1) {
             if (parseFloat(ds.priceChangePercent) > changePercent) {
-                console.log(ds);
                 const alarmIcon = '🚨'.repeat(parseInt(ds.priceChangePercent));
-                const msgToSend = 
-                `*Symbol ${ds.symbol}*\n${alarmIcon}\nPrice \$${ds.lastPrice}\nQuantity ${ds.lastQty}\nPrice change percent ${ds.priceChangePercent}%`
-                console.log('msgToSend', msgToSend);
-                
+                const msgToSend =
+                    `*Symbol ${ds.symbol}*\n${alarmIcon}\nPrice \$${ds.lastPrice}\nQuantity ${ds.lastQty}\nPrice change percent ${ds.priceChangePercent}%`;
                 bot.sendMessage(msg.chat.id, msgToSend, { parse_mode: "Markdown" })
             }
         }
@@ -52,7 +37,7 @@ bot.onText(/\/tothemoon/, async (msg) => {
 
 bot.onText(/\/help/, async (msg) => {
     bot.sendMessage(msg.chat.id,
-        "\/help Show available commands.\n\/test Test connectivity.\n\/tothemoon Showing coins on the moon!");
+        "\/help Show available commands.\n\/test Test connectivity.\n\/tothemoon <limit-percent> Showing coins on the moon!");
 });
 
 export default bot;
